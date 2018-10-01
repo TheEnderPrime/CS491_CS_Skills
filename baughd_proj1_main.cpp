20eafedc88bcd3309658df1188b4ceed2d6b9352
David B <dbaugh.oregon@gmail.com>
2018-10-01 13:49:29 -0700

    finished initial code, now to create main to test

--- baughd_proj1_main.cpp
+++ baughd_proj1_main.cpp
@@ -13,40 +13,46 @@
 glm::vec3
 WhatPartOfALivesInBDir( glm::vec3 a, glm::vec3 b )
 {
-	return dot(a, normalize(b))
+	return a * glm::dot( glm::normalize(a), glm::normalize(b) );
 }
 
 
 glm::vec3
 WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
 {
-//	return magnitude of (cross(A, normalize(B))) 
+	return a * glm::length( glm::cross( glm::normalize(a), glm::normalize(b) ) );
 }
 
 
 glm::vec3
 UnitSurfaceNormal( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-//	return (cross(R-Q),(S-Q)
+	return glm::cross((r-q),(s-q));
 }
 
 
 float
 Area( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-//	return 0.5 * mag(cross(R-Q),(S-Q))
+	return 0.5 * glm::length(glm::cross((r-q),(s-q)));
 }
 
 
 bool
 IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-	//n = cross((R-Q),(s-Q))
-	//nq = cross((R-Q),(P-Q))
-	//nr = cross((S-R),(P-R))
-	//ns = cross((Q-S),(P-S))
-	//if -> dot(n,nq), dot(n,nr), dot(n,ns) > 0 then within triangle
-//	return 
+	glm::vec3 n = cross((r-q),(s-q));
+	glm::vec3 nq = cross((r-q),(p-q));
+	glm::vec3 nr = cross((s-r),(p-r));
+	glm::vec3 ns = cross((q-s),(p-s));
+	if (dot(n,nq) > 0, dot(n,nr) > 0, dot(n,ns) > 0)
+	{
+		return true;
+	} 
+	else
+	{
+		return false;
+	} 
 }
 
 
@@ -54,7 +60,7 @@ IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 float
 DistanceFromPointToPlane( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-//	d = dot((P - Q), unit normal)
+	return glm::dot((p - q), glm::normalize(q));
 }
 
 
