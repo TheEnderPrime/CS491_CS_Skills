3dfbd8fec555d788c51bddc3382846b59b6e5a7a
David B <dbaugh.oregon@gmail.com>
2018-10-01 14:29:51 -0700

    Finished, I think. Needs to be double checked and have main removed

--- baughd_proj1_main.cpp
+++ baughd_proj1_main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <iostream>
+
+using namespace std;
 
 #define GLM_FORCE_RADIANS
 #include "glm/vec2.hpp"
@@ -13,9 +16,8 @@
 glm::vec3
 WhatPartOfALivesInBDir( glm::vec3 a, glm::vec3 b )
 {
-	return a * glm::dot( glm::normalize(a), glm::normalize(b) );
+	return (a * glm::dot( glm::normalize(a), glm::normalize(b) ));
 }
-
 
 glm::vec3
 WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
@@ -23,20 +25,17 @@ WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
 	return a * glm::length( glm::cross( glm::normalize(a), glm::normalize(b) ) );
 }
 
-
 glm::vec3
 UnitSurfaceNormal( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
 	return glm::cross((r-q),(s-q));
 }
 
-
 float
 Area( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
 	return 0.5 * glm::length(glm::cross((r-q),(s-q)));
 }
-
 
 bool
 IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
@@ -55,18 +54,29 @@ IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 	} 
 }
 
-
-
 float
 DistanceFromPointToPlane( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
 	return glm::dot((p - q), glm::normalize(q));
 }
 
-
 void
 WhoAmI( std::string &yourName, std::string &yourEmailAddress )
 {
 	yourName = "David Baugh";
 	yourEmailAddress = "baughd@oregonstate.edu" ;
+}
+
+int main()
+{
+	glm::vec3 a = glm::vec3(1,10,1);
+	glm::vec3 b = glm::vec3(6,1,13);
+	glm::vec3 c = glm::vec3(11,11,1);
+	glm::vec3 d = glm::vec3(1,1,1);
+	glm::vec3 vecAnswer = WhatPartOfALivesInBDir(a, b);
+	float floatAnswer = DistanceFromPointToPlane(a, b, c, d);
+	bool boolAnswer = IsPointInTriangle(a, b, c, d);
+	cout << boolAnswer;
+	//cout << vecAnswer.x << " " << vecAnswer.y << " " << vecAnswer.z;
+	return 0;
 }
